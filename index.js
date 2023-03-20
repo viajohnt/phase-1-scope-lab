5ea53e1a8eab14ffc78596fc38b12d880d495ae6
John Truong <viajohnt@gmail.com>
2023-03-20 14:18:53 -0600

    WoW

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const LeastFavoriteCustomer = 'tammy'
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = 's'
+}
